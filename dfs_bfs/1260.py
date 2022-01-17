b018869b5b79e9a2e67946c5f650c7f163d0312a
honeyhyuni <tjfkqwkd001@naver.com>
2022-01-17 17:31:12 +0900

    DFS와 BFS

---
+++ dfs_bfs/1260.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited[v] = True
+    while q:
+        x = q.popleft()
+        print(x, end=' ')
+        for i in range(1, n + 1):
+            if not visited[i] and arr[x][i] == 1:
+                visited[i] = True
+                q.append(i)
+
+
+def dfs(v):
+    visited2[v] = True
+    print(v, end=' ')
+    for i in range(1, n + 1):
+        if not visited2[i] and arr[v][i] == 1:
+            dfs(i)
+
+
+n, m, v = map(int, sys.stdin.readline().split())
+arr = [[0] * (n + 1) for _ in range(n + 1)]
+visited = [False] * (n + 1)
+visited2 = [False] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    arr[a][b] = arr[b][a] = 1
+dfs(v)
+print()
+bfs(v)
