71b84e576b02de9c16d0521b12541117154492ae
honeyhyuni <tjfkqwkd001@naver.com>
2022-01-22 17:55:19 +0900

    저항

---
+++ 구현/1076.py
@@ -0,0 +1,6 @@
+arr={'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5,
+     'blue': 6, 'violet': 7, 'grey': 8, 'white': 9}
+x = input()
+y = input()
+z = input()
+print((10*arr[x]+arr[y])*(10**arr[z]))
