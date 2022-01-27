e18c10633a418d693c1cdd5dfb7d95d7e65cc4f8
honeyhyuni <tjfkqwkd001@naver.com>
2022-01-27 16:07:39 +0900

    좌표 정렬하기

---
+++ sort/11650.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+a = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+
+a.sort(key= lambda x: (x[0], x[1]))
+
+for i in a:
+    print(*i)
