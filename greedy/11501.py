b05a757b4d07cf2f3278b4e4d843061082be3546
honeyhyuni <tjfkqwkd001@naver.com>
2022-01-28 14:01:57 +0900

    주식

---
+++ greedy/11501.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/11501
+import sys
+input = sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    value = 0
+    max = 0
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] > max:
+            max = arr[i]
+        else:
+            value += max - arr[i]
+    print(value)
