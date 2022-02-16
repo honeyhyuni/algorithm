f361d728ffde5c8762bf33c206fbad460c6d6445
honeyhyuni <tjfkqwkd001@naver.com>
2022-02-16 18:12:23 +0900

    1, 2, 3 더하기

---
+++ dynamic programming/9095.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/9095
+t = int(input())
+
+dp = [1, 2, 4]
+
+for i in range(3, 10):
+    dp.append(dp[i - 3] + dp[i - 2] + dp[i - 1])
+for i in range(t):
+    result = int(input())
+    print(dp[result - 1])
