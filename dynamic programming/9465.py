268c502850427547860fe3b199423cf9b4a8e961
honeyhyuni <tjfkqwkd001@naver.com>
2022-02-21 19:21:51 +0900

    스티커

---
+++ dynamic programming/9465.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/9465
+import sys
+input = sys.stdin.readline
+T = int(input())
+for t in range(T):
+    n = int(input())
+    arr = []
+    for i in range(2):
+        arr.append(list(map(int, input().split())))
+    for i in range(1, n):
+        if i == 1:
+            arr[0][i] += arr[1][i-1]
+            arr[1][i] += arr[0][i-1]
+        else:
+            arr[0][i] += max(arr[1][i-1], arr[1][i-2])
+            arr[1][i] += max(arr[0][i-1], arr[0][i-2])
+    print(max(arr[0][n-1], arr[1][n-1]))
