da06d61a190f525cb0d5b37881585130565c00cb
honeyhyuni <tjfkqwkd001@naver.com>
2022-02-27 17:56:05 +0900

    구간 합 구하기 5

---
+++ dynamic programming/11660.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/11660
+import sys
+input = sys.stdin.readline
+n, M = map(int, input().split())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+dp = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] += arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+
+for i in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(dp[x2][y2] - (dp[x1-1][y2] + dp[x2][y1-1] - dp[x1-1][y1-1]))
