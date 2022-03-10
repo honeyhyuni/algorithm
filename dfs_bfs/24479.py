811dfd7989b4784c5a84cc15e202fc68b6cd8194
honeyhyuni <tjfkqwkd001@naver.com>
2022-03-10 19:42:33 +0900

    알고리즘 수업 - 깊이 우선 탐색1

---
+++ dfs_bfs/24479.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/24479
+# dfs 알고리즘 이론 문제
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+
+def dfs(v):
+    global cnt
+    visited[v] = cnt
+    for _ in arr[v]:
+        if visited[_] == 0:
+            cnt += 1
+            dfs(_)
+
+
+n, m, r = map(int, input().split())
+arr = [[] for i in range(n + 1)]
+for i in range(m):
+    x, y = map(int, input().split())
+    arr[x].append(y)
+    arr[y].append(x)
+visited = [0] * (n + 1)
+cnt = 1
+for i in arr:
+    i.sort()
+dfs(r)
+print("\n".join(map(str, visited[1:])))
