840c4c1b382c9855f3a7e5ed07cd6171bdbd79d4
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-03 18:28:58 +0900

    다트 게임

---
+++ programmers_level1/dartgame.py
@@ -0,0 +1,26 @@
+def solution(dartResult):
+    answer = []
+    dic = {"S": 1, "D": 2, "T": 3}
+    temp = ""
+    for i in dartResult:
+        if i in dic:
+            answer.append(pow(int(temp), dic[i]))
+            temp = ""
+        elif i == "*":
+            if len(answer) > 1:
+                answer[-2] *= 2
+            answer[-1] *= 2
+        elif i == "#":
+            answer[-1] *= -1
+        else:
+            temp += i
+    return sum(answer)
+
+
+print(solution("1S2D*3T"))
+print(solution("1D2S#10S"))
+print(solution("1D2S0T"))
+print(solution("1S*2T*3S"))
+print(solution("1D#2S*3S"))
+print(solution("1T2D3D#"))
+print(solution("1D2S3T*"))
