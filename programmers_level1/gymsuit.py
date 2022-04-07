69558a6eef403b3be9a6a56d4dfbe3e8aaeb9d75
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-07 15:56:14 +0900

    체육복

---
+++ programmers_level1/gymsuit.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42862
+def solution(n, lost, reserve):
+    s_lost = set(lost) - set(reserve)
+    s_reserve = set(reserve) - set(lost)
+
+    for i in s_reserve:
+        if i-1 in s_lost:
+            s_lost.remove(i-1)
+        elif i+1 in s_lost:
+            s_lost.remove(i+1)
+    return n-len(s_lost)
