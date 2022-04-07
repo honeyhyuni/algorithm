dd09ab35649eea7502133c8039ee52078c0bd35b
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-07 15:45:13 +0900

    없는 숫자 더하기

---
+++ programmers_level1/sumnotinnumber.py
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/86051
+def solution(numbers):
+    answer = 0
+    for i in range(10):
+        if i not in numbers:
+            answer += i
+    return answer
