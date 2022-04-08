7527fedf52b15938bbfebe9e2fa8a2471911348f
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-08 16:54:22 +0900

    소수 찾기

---
+++ programmers_level1/findPrimeNumber.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12921
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num -= set(range(2*i,n+1,i))
+    return len(num)
+
+
+print(solution(10))
+print(solution(5))
