eb4a8e69ef362afa1d880fe3b41958b011b5534e
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-10 18:29:06 +0900

    자릿수 더하기

---
+++ programmers_level1/sumLenN.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12931
+def solution(n):
+    answer = 0
+    for i in str(n):
+        answer += int(i)
+    return answer
+
+print(solution(123))
+print(solution(987))
