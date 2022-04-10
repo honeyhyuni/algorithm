c113081cb4bc9c6586f729d64abc1c3a3b4f1a46
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-10 18:26:36 +0900

    이상한 문자 만들기

---
+++ programmers_level1/weirdString.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12930
+def solution(s):
+    answer = ''
+    new_list = s.split(' ')
+    for i in new_list:
+        for j in range(len(i)):
+            if j % 2 == 0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer+= ' '
+    return answer[0:-1]
+
+
+print(solution("Try hEllo world"))
