53647c825ae353ccc432c89ce155b9877e872752
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-10 19:14:00 +0900

    콜라츠 추측

---
+++ programmers_level1/colates.py
@@ -0,0 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12943
+def solution(num):
+    for i in range(500):
+        if num == 1:
+            return i
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num * 3 + 1
+    return -1
+
+print(solution(6))
+print(solution(16))
+print(solution(626331))
