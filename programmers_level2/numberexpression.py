b5afd65ffa73386aa7feb9baf6c9d3c846f3acaa
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-13 16:11:45 +0900

    숫자의 표현

---
+++ programmers_level2/numberexpression.py
@@ -0,0 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12924
+def solution(n):
+    answer = 0
+    for i in range(1, n + 1):
+        s = 0
+        while s < n:
+            s += i
+            i += 1
+        if s == n:
+            answer += 1
+    return answer
+
+
+print(solution(15))
