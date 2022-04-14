e3f975d46b883ec3a7b30d5c9ddadf44034a7e26
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-14 17:25:45 +0900

    가장 큰 정사각형 찾기

---
+++ programmers_level2/findSquare.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12905
+def solution(board):
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
+            if board[i][j] == 1:
+                board[i][j] = min(board[i-1][j], board[i][j-1], board[i-1][j-1]) + 1
+
+    return max(map(max, board)) ** 2
+
+print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
+print(solution([[0,0,1,1],[1,1,1,1]]))
