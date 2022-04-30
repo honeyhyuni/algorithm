9952806a00e7a03c157546402206b405cc3bb1a2
honeyhyuni <tjfkqwkd001@naver.com>
2022-04-30 17:41:04 +0900

    부등호

--- etc/2529.py
+++ etc/2529.py
@@ -1,33 +1,39 @@
 # https://www.acmicpc.net/problem/2529
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-comp = list(map(str, input().split()))
-st = ""
-visited = [False] * 10
-max_v = ""
-min_v = ""
+compare = list(map(str, input().split()))
+arr = []
 
 
-def check(i, j, k):
-    if k == ">":
-        return i > j
-    else:
-        return i < j
-
-
-def back(cnt, s):
-    global max_v, min_v
-    if cnt > n:
-        if len(min_v) == 0:
-            min_v = s
+def check():
+    if len(arr) >= 2:
+        if compare[len(arr) - 2] == "<":
+            if arr[-2] > arr[-1]:
+                return False
         else:
-            max_v = s
-        return
+            if arr[-2] < arr[-1]:
+                return False
+    if len(arr) == n + 1:
+        result.append("".join(map(str, arr)))
+        return False
+    return True
+
+
+result = []
+
+
+def back():
     for i in range(10):
-        if not visited[i]:
-            if cnt == 0 or check(s[-1], str(i), comp[cnt-1]):
-                visited[i] = True
-                back(cnt+1, s + str(i))
-                visited[i] = False
-back(0, st)
-print(max_v)
-print(min_v)
+        if i in arr:
+            continue
+        arr.append(i)
+        if check():
+            back()
+        arr.pop()
+
+
+back()
+print(result[-1])
+print(result[0])
