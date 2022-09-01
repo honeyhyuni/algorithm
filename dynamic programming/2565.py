3c34848d62e4b345677cabd8bd25245fd6f93cc8
honeyhyuni <tjfkqwkd001@naver.com>
2022-09-01 18:06:18 +0900

    전깃줄

---
+++ dynamic programming/2565.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/2565
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = sorted([list(map(int, input().split())) for i in range(n)])
+dp = [1] * n
+for i in range(n):
+    for j in range(i):
+        if arr[i][1] > arr[j][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(n - max(dp))
